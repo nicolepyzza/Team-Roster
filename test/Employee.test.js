76281f5bffb21d3da59b5f7ae404ab51b61095b3
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-05-05 18:03:54 -0400

    testing

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -10,7 +10,7 @@ test("Can set name via constructor arguments", () => {
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
-
+  
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
